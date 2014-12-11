01fc64aed3bfcaa14efb5a4f4fe414d82df8b562
Russell Wolf <russellmwolf@gmail.com>
2014-12-11 13:39:48 -0500

    Finished functionality for 1 field deep, some refactoring

--- nrswolf:observatory.js
+++ nrswolf:observatory.js
@@ -1,9 +1,17 @@
 Observatory = {
-  buildCSV: function(collection) {
+  buildOutput: function(collection) {
     var output = [];
-    var headers = this.buildHeaders(collection);
 
+    var headers = this.buildHeaders(collection);
     output.push(headers);
+
+    var that = this;
+    collection.forEach(function(item, _index) {
+      var itemData = that.buildItemData(item, headers);
+      output.push(itemData);
+    });
+
+    return output;
   },
 
   buildHeaders: function(collection) {
@@ -20,5 +28,35 @@ Observatory = {
     });
 
     return headers;
+  },
+
+  buildItemData: function(item, headers) {
+    var itemData = [];
+
+    headers.forEach(function(field, _index) {
+      if (!!item[field]) {
+        itemData.push(item[field]);
+      }
+    });
+
+    return itemData;
+  },
+
+  buildCSV: function(collection) {
+    var outputData = this.buildOutput(collection);
+    var csvContent = "data:text/csv;charset=utf-8,";
+
+    outputData.forEach(function(infoArray, index) {
+      dataString = infoArray.join(",");
+      csvContent += index < infoArray.length ? dataString+ "\n" : dataString;
+    });
+
+    var encodedUri = encodeURI(csvContent);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "Winview_Users.csv");
+
+    link.click();
   }
 };
