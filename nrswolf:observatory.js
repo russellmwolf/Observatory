5566f62ac5d63237c20141a2e61905e3c2fbd722
Russell Wolf <russellmwolf@gmail.com>
2014-12-11 13:15:35 -0500

    Get collection headers

--- nrswolf:observatory.js
+++ nrswolf:observatory.js
@@ -1,1 +1,24 @@
-// Write your package code here!
+Observatory = {
+  buildCSV: function(collection) {
+    var output = [];
+    var headers = this.buildHeaders(collection);
+
+    output.push(headers);
+  },
+
+  buildHeaders: function(collection) {
+    var headers = [];
+
+    collection.forEach(function(item, _index) {
+      for (var key in item) {
+        if (headers.indexOf(key) === 1) {
+          break;
+        }
+
+        headers.push(key);
+      }
+    });
+
+    return headers;
+  }
+};
