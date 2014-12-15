76a1d75451bb8c7eefb36822b0e5349b00a8c484
Russell Wolf <russellmwolf@gmail.com>
2014-12-15 14:42:20 -0500

    CSV building now parses nested arrays and objects

--- nrswolf:observatory.js
+++ nrswolf:observatory.js
@@ -18,7 +18,9 @@ Observatory = {
     var headers = [];
 
     collection.forEach(function(item, _index) {
-      for (var key in item) {
+      flatItem = flatten(item);
+
+      for (var key in flatItem) {
         if (headers.indexOf(key) === -1) {
           headers.push(key);
         }
@@ -32,11 +34,9 @@ Observatory = {
     var itemData = [];
 
     headers.forEach(function(field, _index) {
-      if (!!item[field]) {
-        itemData.push(item[field]);
-      } else {
-        itemData.push(null);
-      }
+      try {
+        itemData.push(objectReference(item, field));
+      } catch(err){}
     });
 
     return itemData;
@@ -54,11 +54,54 @@ Observatory = {
     });
 
     var encodedUri = encodeURI(csvContent);
-
+    
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
     link.setAttribute("download", name + ".csv");
 
     link.click();
+  },
+};
+
+// https://github.com/hughsk/flat
+var flatten = function(target, opts) {
+  opts = opts || {};
+
+  var delimiter = opts.delimiter || '.';
+  var output = {};
+
+  function step(object, prev) {
+    Object.keys(object).forEach(function(key) {
+      var value = object[key];
+      var isarray = opts.safe && Array.isArray(value);
+      var type = Object.prototype.toString.call(value);
+      var isobject = (
+        type === "[object Object]" ||
+        type === "[object Array]"
+      );
+
+      var newKey = prev ? prev + delimiter + key : key;
+
+      if (!isarray && isobject) {
+        return step(value, newKey);
+      }
+
+      output[newKey] = value;
+    });
   }
+
+  step(target);
+
+  return output;
+};
+
+// http://scott.donnel.ly/javascript-function-to-convert-a-string-in-dot-andor-array-notation-into-a-reference/
+var objectReference = function (object, reference) {
+  function arr_deref(o, ref, i) {
+    return !ref ? o : (o[ref.slice(0, i ? -1 : ref.length)]);
+  }
+  function dot_deref(o, ref) {
+    return !ref ? o : ref.split('[').reduce(arr_deref, o);
+  }
+  return reference.split('.').reduce(dot_deref, object);
 };
